a6a5d56487908d4457faf7188e8b0b0c0806a889
eggxp <eggxxp@gmail.com>
2012-05-29 09:18:39 +0800

    更新到2007

--- SuperParse2007.cpp
+++ SuperParse2007.cpp
@@ -2,9 +2,9 @@
 #include <vcl.h>
 #pragma hdrstop
 //---------------------------------------------------------------------
-USEFORM("about.cpp", AboutBox);
-USEFORM("CHILDWIN.cpp", MDIChild);
 USEFORM("MAIN.CPP", MainForm);
+USEFORM("CHILDWIN.CPP", MDIChild);
+USEFORM("about.cpp", AboutBox);
 USEFORM("UI\FrmConfig.cpp", ConfigFrm);
 USEFORM("UI\FrmDebug.cpp", DebugFrm);
 USEFORM("UI\FrmDesign.cpp", DesignFrm);
@@ -25,38 +25,38 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	Application->Initialize();
 	Application->CreateForm(__classid(TMainForm), &MainForm);
-		Application->CreateForm(__classid(TAboutBox), &AboutBox);
-		Application->CreateForm(__classid(TGoToFrm), &GoToFrm);
-		Application->CreateForm(__classid(TSelectFrm), &SelectFrm);
-		Application->CreateForm(__classid(TTranslationFrm), &TranslationFrm);
-		Application->CreateForm(__classid(TLogFrm), &LogFrm);
-		Application->CreateForm(__classid(TSourceEditFrm), &SourceEditFrm);
-		Application->CreateForm(__classid(TEditFileDescribeFrm), &EditFileDescribeFrm);
-		Application->CreateForm(__classid(TPluginFrm), &PluginFrm);
-		Application->CreateForm(__classid(TListParseFrm), &ListParseFrm);
-		Application->CreateForm(__classid(TSearchFilesFrm), &SearchFilesFrm);
-		Application->CreateForm(__classid(TSearchFrm), &SearchFrm);
-		Application->CreateForm(__classid(TDesignFrm), &DesignFrm);
-		Application->CreateForm(__classid(TSourceGenDesignFrm), &SourceGenDesignFrm);
-		Application->CreateForm(__classid(TDebugFrm), &DebugFrm);
-		Application->CreateForm(__classid(TConfigFrm), &ConfigFrm);
-		Application->CreateForm(__classid(TSetupFrm), &SetupFrm);
-		Application->CreateForm(__classid(TConfigFrm), &ConfigFrm);
-		Application->CreateForm(__classid(TDebugFrm), &DebugFrm);
-		Application->CreateForm(__classid(TDesignFrm), &DesignFrm);
-		Application->CreateForm(__classid(TEditFileDescribeFrm), &EditFileDescribeFrm);
-		Application->CreateForm(__classid(TGoToFrm), &GoToFrm);
-		Application->CreateForm(__classid(TListParseFrm), &ListParseFrm);
-		Application->CreateForm(__classid(TLogFrm), &LogFrm);
-		Application->CreateForm(__classid(TPluginFrm), &PluginFrm);
-		Application->CreateForm(__classid(TSearchFrm), &SearchFrm);
-		Application->CreateForm(__classid(TSearchFilesFrm), &SearchFilesFrm);
-		Application->CreateForm(__classid(TSelectFrm), &SelectFrm);
-		Application->CreateForm(__classid(TSetupFrm), &SetupFrm);
-		Application->CreateForm(__classid(TSourceEditFrm), &SourceEditFrm);
-		Application->CreateForm(__classid(TSourceGenDesignFrm), &SourceGenDesignFrm);
-		Application->CreateForm(__classid(TTranslationFrm), &TranslationFrm);
-		Application->Run();
+         Application->CreateForm(__classid(TAboutBox), &AboutBox);
+         Application->CreateForm(__classid(TGoToFrm), &GoToFrm);
+         Application->CreateForm(__classid(TSelectFrm), &SelectFrm);
+         Application->CreateForm(__classid(TTranslationFrm), &TranslationFrm);
+         Application->CreateForm(__classid(TLogFrm), &LogFrm);
+         Application->CreateForm(__classid(TSourceEditFrm), &SourceEditFrm);
+         Application->CreateForm(__classid(TEditFileDescribeFrm), &EditFileDescribeFrm);
+         Application->CreateForm(__classid(TPluginFrm), &PluginFrm);
+         Application->CreateForm(__classid(TListParseFrm), &ListParseFrm);
+         Application->CreateForm(__classid(TSearchFilesFrm), &SearchFilesFrm);
+         Application->CreateForm(__classid(TSearchFrm), &SearchFrm);
+         Application->CreateForm(__classid(TDesignFrm), &DesignFrm);
+         Application->CreateForm(__classid(TSourceGenDesignFrm), &SourceGenDesignFrm);
+         Application->CreateForm(__classid(TDebugFrm), &DebugFrm);
+         Application->CreateForm(__classid(TConfigFrm), &ConfigFrm);
+         Application->CreateForm(__classid(TSetupFrm), &SetupFrm);
+         Application->CreateForm(__classid(TConfigFrm), &ConfigFrm);
+         Application->CreateForm(__classid(TDebugFrm), &DebugFrm);
+         Application->CreateForm(__classid(TDesignFrm), &DesignFrm);
+         Application->CreateForm(__classid(TEditFileDescribeFrm), &EditFileDescribeFrm);
+         Application->CreateForm(__classid(TGoToFrm), &GoToFrm);
+         Application->CreateForm(__classid(TListParseFrm), &ListParseFrm);
+         Application->CreateForm(__classid(TLogFrm), &LogFrm);
+         Application->CreateForm(__classid(TPluginFrm), &PluginFrm);
+         Application->CreateForm(__classid(TSearchFrm), &SearchFrm);
+         Application->CreateForm(__classid(TSearchFilesFrm), &SearchFilesFrm);
+         Application->CreateForm(__classid(TSelectFrm), &SelectFrm);
+         Application->CreateForm(__classid(TSetupFrm), &SetupFrm);
+         Application->CreateForm(__classid(TSourceEditFrm), &SourceEditFrm);
+         Application->CreateForm(__classid(TSourceGenDesignFrm), &SourceGenDesignFrm);
+         Application->CreateForm(__classid(TTranslationFrm), &TranslationFrm);
+         Application->Run();
 
 	return 0;
 }
